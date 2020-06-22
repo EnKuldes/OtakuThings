528dca97f70ee3faafd4ef465657b835c497d844
Farhan En Muzaki <fmuzaki12@gmail.com>
2020-06-22 20:20:35 +0700

    Nambahin Module Authentikasi Laravel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
+            // Informasi Akun
             $table->string('name');
             $table->string('email')->unique();
+            // Informasi Akun
+            
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
