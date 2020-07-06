28b5301270ba290e7ffb34247df6628fdab0cb37
Farhan En Muzaki <fmuzaki12@gmail.com>
2020-07-06 18:25:35 +0700

    Pembaharuan Field Login dari Email menjadi Username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,12 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+// Handle Request
+use Illuminate\Http\Request;
+
+// Hubung ke DB untuk ambil page pertama
+use DB;
+
 class LoginController extends Controller
 {
     /*
@@ -36,5 +42,22 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    // Overriding Func Username dari yang tadinya required nya Email menjadi Username
+    public function username()
+    {
+        return 'username';
+    }
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        //return $request->only($this->username(), 'password');
+        return ['username' => $request->{$this->username()}, 'password' => $request->password, 'is_enabled' => '1'];
     }
 }
